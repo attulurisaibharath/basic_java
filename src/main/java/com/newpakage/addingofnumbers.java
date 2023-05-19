555aad6fdfad2d35af89c4d17009656dbb9de559
bharathchowdary <Saibharath0459@gmail.com>
2023-05-18 21:13:35 -0500

    adding of two numbers code

---
+++ src/main/java/com/newpakage/addingofnumbers.java
@@ -0,0 +1,12 @@
+package com.newpakage;
+
+public class addingofnumbers {
+    public static void add(String args[]){
+        int a = 11;
+        int b= 12;
+        int c= a+b;
+        System.out.println("The sum of two numbers is:"+c);
+
+    }
+}
+
